d3ff337433cc37e4d557f1cf99c616f25526d2b1
yanshen <yanshen0406@gmail.com>
2024-06-17 14:54:49 -0500

    122

---
+++ 122. Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if len(prices) <= 1:
+            return 0
+        profit = 0
+
+        for i in range(0, len(prices)-1):
+            if prices[i] < prices[i+1]:
+                profit += prices[i+1] - prices[i]
+
+        return profit
+
+
