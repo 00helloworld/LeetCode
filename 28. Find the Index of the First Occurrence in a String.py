471daaf7e1331219d8305cc54f7af60acaa6aaa9
yanshen <yanshen0406@gmail.com>
2024-06-24 12:20:22 -0500

    28

--- 28. Find the Index of the First Occurrence in a String.py
+++ 28. Find the Index of the First Occurrence in a String.py
@@ -6,26 +6,32 @@ class Solution(object):
         :rtype: int
         """
         
-        i = 0
-        j = 0
-        res = -1
+        # i = 0
+        # j = 0
+        # res = -1
 
-        if haystack == needle:
-            return 0
+        # if haystack == needle:
+        #     return 0
 
-        while i < len(haystack) and j < len(needle):
-            if haystack[i] == needle[j]:
-                if j == 0:
-                    res = i
-                if j == len(needle) - 1:
-                    return res
-                i += 1
-                j += 1
-            else:
-                i = i+1 if res==-1 else res+1
-                res = -1
-                j = 0
+        # while i < len(haystack) and j < len(needle):
+        #     if haystack[i] == needle[j]:
+        #         if j == 0:
+        #             res = i
+        #         if j == len(needle) - 1:
+        #             return res
+        #         i += 1
+        #         j += 1
+        #     else:
+        #         i = i+1 if res==-1 else res+1
+        #         res = -1
+        #         j = 0
                 
+        # return -1
+    
+        # Tricky
+        for i in range(len(haystack)):
+            if haystack[i:i+len(needle)] == needle:
+                return i
         return -1
 
 
