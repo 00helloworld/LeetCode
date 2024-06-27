58b0ca85ff0d7c29a66a54316af465434da2effa
yanshen <yanshen0406@gmail.com>
2024-06-27 12:58:39 -0500

    125

---
+++ 125. Valid Palindrome.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        i = 0
+        j = len(s) - 1
+
+        while i <= j:
+            if s[i].isalnum() and s[j].isalnum():
+                if s[i].lower() != s[j].lower():
+                    return False
+                else:
+                    i += 1
+                    j -= 1
+            elif not s[i].isalnum():
+                i += 1
+            else:
+                j -= 1
+
+        return True
+
+
+        
