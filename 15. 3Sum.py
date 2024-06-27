bdc5cf05816125f5a9579360d33a4fed985da94d
yanshen <yanshen0406@gmail.com>
2024-06-27 15:55:17 -0500

    15

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -5,3 +5,22 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         
+        # Sort + Two Pointers
+        res = set()
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                break
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                if nums[i] + nums[j] + nums[k] < 0:
+                    j += 1
+                elif nums[i] + nums[j] + nums[k] > 0:
+                    k -= 1
+                else:
+                    res.add((nums[i], nums[j], nums[k]))
+                    j += 1
+                    k -= 1
+
+        return list(res)
