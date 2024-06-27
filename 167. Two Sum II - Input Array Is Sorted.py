2c7436008c52e3079ef731e2072a9a6aca2d9e58
yanshen <yanshen0406@gmail.com>
2024-06-27 15:32:42 -0500

    167

--- 167. Two Sum II - Input Array Is Sorted.py
+++ 167. Two Sum II - Input Array Is Sorted.py
@@ -5,4 +5,16 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        
+        # Two Pointers
+        i = 0
+        j = len(numbers) - 1
+
+        while i < j:
+            if numbers[i] + numbers[j] < target:
+                i += 1
+            elif numbers[i] + numbers[j] > target:
+                j -= 1
+            else:
+                break
+
+        return [i+1, j+1]
