ffa025f0d3490e1be51ac89b0d7513e90fe92343
yanshen <yanshen0406@gmail.com>
2024-07-08 17:50:19 -0500

    73

---
+++ 73. Set Matrix Zeroes.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        
+        column_arr = set()
+        row_arr = set()
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    column_arr.add(j)
+                    row_arr.add(i)
+                
+        for r in row_arr:
+            for j in range(len(matrix[0])):
+                matrix[r][j] = 0
+
+        for c in column_arr:
+            for i in range(len(matrix[0])):
+                matrix[i][c] = 0
+
+                
+
+
+                
